f04484ab1ae00a74064d8965d998c806adf8223f
rubydusa <rubydusa@protonmail.com>
2025-02-24 09:02:12 +0200

    fix datasmith

--- datasmith/src/main.rs
+++ datasmith/src/main.rs
@@ -1,13 +1,13 @@
 //! Example of using the HTTP provider using the `on_http` method.
 
 use alloy::providers::{Provider, ProviderBuilder};
-use eyre::Result;
+use anyhow::Result;
 
 #[tokio::main]
-async fn main() -> eyre::Result<()> {
+async fn main() -> Result<()> {
     let rpc_url = "https://holesky.drpc.org".parse()?;
     let provider = ProviderBuilder::new().on_http(rpc_url);
-    provider.trace_transaction();
+    // provider.trace_transaction();
 
     Ok(())
 }
